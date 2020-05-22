1c8ba70a365e79f09f9d5277428f356d1d0a8075
Aakash Sanwgan <aakashssangwan024@gmail.com>
2020-05-23 01:15:01 +0530

    adding footer  cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-
+import nav  from "./components/nav";
 import header from "./components/header";
 import HeroBanner from "./components/Hero-banner" ;
+import Cards from "./components/cards";
 // import logo from './logo.svg';
 import './App.css';
 
@@ -9,10 +10,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
-        
-          <HeroBanner />
+          <nav />
           <header/>
+          <HeroBanner />
+          <Cards/>
+          
+
  
     </div>
   );
---
+++ src/components/cards.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./css/Cards.css";
+import {Card,CardImg,CardText,CardBody,CardTitle,CardSubtitle} from 'reactstrap'
+const Cards = () => (
+<div>
+
+
+
+
+</div>
+);
+
+export default Cards;
