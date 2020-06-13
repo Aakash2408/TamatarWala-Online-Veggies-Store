99dcab0675f278d47be95427b3e55c83ddcc6164
sarthaksadh01 <sarthaksadh01@gmail.com>
2020-06-13 22:24:23 +0530

    added cloud firestore query

--- src/App.js
+++ src/App.js
@@ -11,24 +11,13 @@ import Sale from './containers/pages/Sale';
 import Cart from './containers/pages/Cart';
 import footer from './containers/pages/footer';
 import Checkout from './containers/pages/Checkout';
-import firebase from "firebase";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import './App.css';
 
-firebase.initializeApp({
-    apiKey: "AIzaSyBEPhxyEU0oTqLR6mjiy4AJOSpmcM_DrcE",
-    authDomain: "online-shop-32976.firebaseapp.com",
-    databaseURL: "https://online-shop-32976.firebaseio.com",
-    projectId: "online-shop-32976",
-    storageBucket: "online-shop-32976.appspot.com",
-    messagingSenderId: "728767128743",
-    appId: "1:728767128743:web:b2621107de1f9dc2c56782",
-    measurementId: "G-S9R8X2962W"
 
-})
 
 class App extends Component {
-    false
+    // false
     //      state = { isSignedIn: false }
     //   uiConfig = {
     //     signInFlow: "popup",
